84f10f871559d770e4c7783ef29be277bf7795ae
06Days (Toby Cammock-Elliott) <63680371+06Days@users.noreply.github.com>
2026-08-24 14:30:39 +1200

    update

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // Accessible at /api
+  SwaggerModule.setup('api', app, document); 
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
