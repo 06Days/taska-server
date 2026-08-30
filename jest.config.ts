280305a49ed003387133c700af9cdd4e794dafdb
06Days (Toby Cammock-Elliott) <63680371+06Days@users.noreply.github.com>
2026-08-31 11:29:52 +1200

    jest impl

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+// // jest.config.ts
+// import { defineConfig } from 'ts-jest';
+
+// export default defineConfig({
+//   testEnvironment: 'node',
+//   testRegex: '.*\\.spec\\.ts$',
+//   moduleFileExtensions: ['ts', 'js'],
+//   transform: {
+//     '^.+\\.ts$': 'ts-jest',
+//   },
+//   testMatch: ['<rootDir>/src/**/*.spec.ts'],
+// });
